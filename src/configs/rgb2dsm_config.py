25b9ed7daa777465467abd4ade0ebdefbeb176f3
Ömer Can Karadağ <krdg.omercan@hotmail.com>
2025-02-06 15:49:42 +0300

    increased batch size to 16, increased epoch to 500

--- src/configs/rgb2dsm_config.py
+++ src/configs/rgb2dsm_config.py
@@ -4,13 +4,13 @@ from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LEARNING_RATE = 1e-3
-BATCH_SIZE = 8
+BATCH_SIZE = 16
 NUM_WORKERS = 4
 IMAGE_SIZE = 512
 CHANNELS_IMG = 3
 L1_LAMBDA = 100
 LAMBDA_GP = 10
-NUM_EPOCHS = 200
+NUM_EPOCHS = 500
 LOAD_MODEL = False
 SAVE_MODEL = True
 CHECKPOINT_DISC = "disc.pth.tar"
