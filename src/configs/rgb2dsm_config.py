cfed76e948fae8ccc94e6f9fdcdab979e51c7e5c
Ömer Can Karadağ <krdg.omercan@hotmail.com>
2025-02-22 13:45:45 +0300

    changed config

--- src/configs/rgb2dsm_config.py
+++ src/configs/rgb2dsm_config.py
@@ -4,20 +4,20 @@ from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LEARNING_RATE = 1e-3
-BATCH_SIZE = 16
+BATCH_SIZE = 8
 NUM_WORKERS = 4
 IMAGE_SIZE = 512
 CHANNELS_IMG = 3
 L1_LAMBDA = 100
 LAMBDA_GP = 10
-NUM_EPOCHS = 500
+NUM_EPOCHS = 200
 LOAD_MODEL = False
 SAVE_MODEL = True
 CHECKPOINT_DISC = "disc.pth.tar"
 CHECKPOINT_GEN = "gen.pth.tar"
 
 both_transform = A.Compose(
-    [A.Resize(width=256, height=256),], additional_targets={"image0": "image"},
+    [A.Resize(width=512, height=512),], additional_targets={"image0": "image"},
 )
 
 transform_only_input = A.Compose(
