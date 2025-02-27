d0fee34a647084d648ff643f4ef8359b6c6327a3
Ömer Can Karadağ <krdg.omercan@hotmail.com>
2025-02-27 17:28:10 +0300

    removed dropout2d from discriminator

--- src/rgb2dsm/training/discriminator.py
+++ src/rgb2dsm/training/discriminator.py
@@ -5,10 +5,9 @@ class CNNBlock(nn.Module):
     def __init__(self,in_channels,out_channels,stride):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.utils.spectral_norm(nn.Conv2d(in_channels,out_channels,4,stride,padding=1, bias=False,padding_mode="reflect")),
+            nn.utils.spectral_norm(nn.Conv2d(in_channels,out_channels,4,stride, bias=False,padding_mode="reflect")),
             nn.InstanceNorm2d(out_channels,affine=True),
             nn.LeakyReLU(0.2),
-            nn.Dropout2d(0.2)
         )
 
     def forward(self,x):
