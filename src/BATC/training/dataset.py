aff93efb11b2eee5d0e3f4283b5a262aa3006ced
Ömer Can Karadağ <krdg.omercan@hotmail.com>
2025-03-13 21:29:22 +0300

    changed maskrcnn dataset loader to skip corrupted data files

--- src/BATC/training/dataset.py
+++ src/BATC/training/dataset.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torch.utils.data import Dataset
 from pycocotools.coco import COCO
@@ -10,12 +11,30 @@ class BuildingDataset(Dataset):
         self.coco = COCO(annotation_path)
         self.image_ids = list(self.coco.imgs.keys())
         self.transform = transform
+
+        # Filter out image IDs that don't exist on disk
+        self.image_ids = [
+            img_id for img_id in self.image_ids
+            if os.path.exists(os.path.join(self.images_dir, self.coco.imgs[img_id]['file_name']))
+        ]
+        
+        print(f"Filtered dataset: {len(self.image_ids)} valid images found.")
+
     def __len__(self):
         return len(self.image_ids)
+
     def __getitem__(self, idx):
         image_id = self.image_ids[idx]
         image_info = self.coco.loadImgs(image_id)[0]
-        image = cv2.imread(f"{self.images_dir}/{image_info['file_name']}")
+        image_path = os.path.join(self.images_dir, image_info['file_name'])
+
+        image = cv2.imread(image_path)
+        
+        # Handle missing or corrupt images
+        if image is None:
+            print(f"Warning: Image {image_path} is missing or corrupt, skipping...")
+            return self.__getitem__((idx + 1) % len(self.image_ids))  # Load next valid image
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         ann_ids = self.coco.getAnnIds(imgIds=image_id)
