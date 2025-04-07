70b602581beae1efb85765d391b135ce923df35a
krdgomer <krdg.omercan@hotmail.com>
2025-04-07 18:47:03 +0300

    .

--- src/models/dsm2dtm.py
+++ src/models/dsm2dtm.py
@@ -5,7 +5,7 @@ from rasterio.transform import from_origin
 from bulldozer.pipeline.bulldozer_pipeline import dsm_to_dtm
 from PIL import Image
 
-def generate_dtm(dsm_path, output_path, kernel_size=49, smooth_sigma=10):
+def generate_dtm(dsm_path, output_path):
     # Load DSM image
     dsm = Image.open(dsm_path)
 
