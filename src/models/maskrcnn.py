6693fac255e465c8e84c385ef2a03a79f3adf8d7
krdgomer <krdg.omercan@hotmail.com>
2025-04-21 17:45:28 +0300

    added docstring to maskrcnn.py

--- src/models/maskrcnn.py
+++ src/models/maskrcnn.py
@@ -7,6 +7,12 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 def get_model():
+    """Load a pre-trained Mask R-CNN model from torchvision and modify it for binary classification.
+    The model is based on a ResNet-50 backbone with a Feature Pyramid Network (FPN) for improved feature extraction.
+
+    Returns:
+        model: A Mask R-CNN model with a ResNet-50 backbone and a Feature Pyramid Network (FPN) for improved feature extraction.
+    """
     model = torchvision.models.detection.maskrcnn_resnet50_fpn()
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features , 2)
