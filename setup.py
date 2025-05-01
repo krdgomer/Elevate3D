96dafc5eaad372fd5492b95e50ceffd2879cf1ee
krdgomer <krdg.omercan@hotmail.com>
2025-05-01 20:49:05 +0300

    0.4.0 setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='elevate3d',
-    version='0.3.0',
+    version='0.4.0',
     author='Ömer Can Karadağ',
     author_email='krdg.omercan@hotmail.com',
     description='3D terrain and structure reconstruction from single RGB images',
